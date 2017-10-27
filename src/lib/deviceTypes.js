4bd0fd128e26e495e1431a3a580c009d548f6f02
horihiro <hori.kawa.shima.tool+github@gmail.com>
2017-10-27 10:04:42 +0900

    export devicetype

---
+++ src/lib/deviceTypes.js
@@ -0,0 +1,67 @@
+export default [
+  {
+    name: 'Ionic',
+    authType: 'AES',
+  },
+  {
+    name: 'Charge 2',
+    authType: 'AES',
+  },
+  {
+    name: 'Alta HR',
+    authType: 'AES',
+  },
+  {
+    name: 'Blaze',
+    authType: 'AES',
+  },
+  {
+    name: 'Flyer',
+  },
+  {
+    name: 'Aria 2',
+    authType: 'AES',
+  },
+  {
+    name: 'Aria',
+  },
+  {
+    name: 'Alta',
+    authType: 'AES',
+  },
+  {
+    name: 'Flex 2',
+    authType: 'AES',
+  },
+  {
+    name: 'Surge',
+    authType: 'AES',
+  },
+  {
+    name: 'Charge HR',
+    authType: 'XTEA',
+  },
+  {
+    name: 'Charge',
+    authType: 'XTEA',
+  },
+  {
+    name: 'Flex',
+    authType: 'XTEA',
+  },
+  {
+    name: 'One',
+    authType: 'XTEA',
+  },
+  {
+    name: 'Zip',
+    authType: 'XTEA',
+  },
+  {
+    name: 'Ultra',
+  },
+  {
+    name: 'Force',
+    authType: 'XTEA',
+  },
+];
--- src/lib/generateBtleCredentials.js
+++ src/lib/generateBtleCredentials.js
@@ -1,5 +1,7 @@
-const axios = require('axios');
-const qs = require('querystring');
+import axios from 'axios';
+import qs from 'querystring';
+import devicesTypes from './deviceTypes';
+
 
 axios.defaults.baseURL = 'https://android-cdn-api.fitbit.com';
 
@@ -10,13 +12,14 @@ const REDIRECT_URI = 'https://horihiro.github.io/node-fitbit-livedata/';
 const AUTHORIZATION = 'Basic MjI4VlNSOjQ1MDY4YTc2Mzc0MDRmYzc5OGEyMDhkNmMxZjI5ZTRm';
 
 const URL_OAUTH = 'https://android-api.fitbit.com/oauth2/token';
-const PATH_DEVICES_TYPES = '/1.1/devices/types.json';
+// const PATH_DEVICES_TYPES = '/1.1/devices/types.json';
 const PATH_USER_DEVICES = '/1/user/-/devices.json';
 const PATH_GENBLECREDS = '/1/user/-/devices/tracker/generateBtleClientAuthCredentials.json';
 
 export default (accounts) => {
   const accountArray = (() => (accounts instanceof Array ? accounts : [accounts]))();
   return Promise.all(accountArray.map((account) => {
+
     const params = (() => {
       const queries = {
         scope: SCOPE,
@@ -38,27 +41,27 @@ export default (accounts) => {
     const options = {
       headers: {
         Authorization: AUTHORIZATION,
+        // Authorization: `Basic ${Buffer.from(`${authInfo.clientId}:${authInfo.clientSecret}`).toString('base64')}`,
       },
     };
     return axios.post(URL_OAUTH, params, options);
   })).then((responses) => {
-    const tokens = responses.map(response => response.data.access_token);
-    const promises = tokens.map(token => axios.get(PATH_USER_DEVICES, {
+    const promises = responses.map(response => axios.get(PATH_USER_DEVICES, {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${response.data.access_token}`,
       },
     }));
-    promises.push(axios.get(PATH_DEVICES_TYPES, {
-      headers: {
-        Authorization: `Bearer ${tokens[0]}`,
-      },
-    }));
+    // promises.push(axios.get(PATH_DEVICES_TYPES, {
+    //   headers: {
+    //     Authorization: `Bearer ${responses[0].data.access_token}`,
+    //   },
+    // }));
     return Promise.all(promises).then(results => ({
       responses: results,
-      tokens,
+      tokens: responses.map(response => response.data.access_token),
     }));
   }).then((results) => {
-    const devicesTypes = results.responses.pop().data.deviceTypes;
+    // const devicesTypes = results.responses.pop().data.deviceTypes;
     const { tokens } = results;
     const allDevices = results.responses.reduce((prev, curr, index) => {
       const userDevices = curr.data;
